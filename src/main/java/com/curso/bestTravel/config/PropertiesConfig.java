9efec5741ca4e9e1ad33dd162e20f632107b8674
Dashk179 <dash_arturo@hotmail.com>
2024-02-19 14:47:24 -0600

    Configurando properties para redis

--- src/main/java/com/curso/bestTravel/config/PropertiesConfig.java
+++ src/main/java/com/curso/bestTravel/config/PropertiesConfig.java
@@ -2,8 +2,13 @@ package com.curso.bestTravel.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource(value = "classpath:configs/api_currently.properties")
+@PropertySources({
+        @PropertySource(value = "classpath:configs/api_currently.properties"),
+        @PropertySource(value = "classpath:configs/redis.properties")
+
+})
 public class PropertiesConfig {
 }
