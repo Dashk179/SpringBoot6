fabfe71f5e20e017ff4b24673475256bfb1a9923
Dashk179 <dash_arturo@hotmail.com>
2024-02-19 14:42:19 -0600

    Creando headers de nuestro excel, llenado excel con datos de postgres, convertyir archivo en bytesy creando contador para la seccion total de ventas

--- src/main/java/com/curso/bestTravel/api/controller/ReportController.java
+++ src/main/java/com/curso/bestTravel/api/controller/ReportController.java
@@ -1,7 +1,14 @@
 package com.curso.bestTravel.api.controller;
 
+import com.curso.bestTravel.infraestructure.abstract_services.ReportService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @Tag(name = "Report")
 public class ReportController {
+    private final ReportService reportService;
+
+    @GetMapping
+    public ResponseEntity<Resource>get(){
+        var headers = new HttpHeaders();
+        headers.setContentType(FORCE_DOWNLOAD);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION,FORCE_DOWNLOAD_HEADER_VALUE);
+
+        var fileinBytes = this.reportService.readfile();
+        ByteArrayResource response  = new ByteArrayResource(fileinBytes);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(fileinBytes.length)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(response);
+    }
+
+    private static final MediaType FORCE_DOWNLOAD=new MediaType("application","force-download");
+    private static final String FORCE_DOWNLOAD_HEADER_VALUE ="attachment; filename=reports.xlsx";
+
 }
--- src/main/java/com/curso/bestTravel/infraestructure/services/ExcelService.java
+++ src/main/java/com/curso/bestTravel/infraestructure/services/ExcelService.java
@@ -1,16 +1,120 @@
 package com.curso.bestTravel.infraestructure.services;
 
+import com.curso.bestTravel.domain.entities.CustomerEntity;
+import com.curso.bestTravel.domain.repository.CustomerRepository;
 import com.curso.bestTravel.infraestructure.abstract_services.ReportService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class ExcelService implements ReportService {
+
+    private final CustomerRepository customerRepository;
     @Override
     public byte[] readfile() {
-        return new byte[0];
+
+        try {
+            this.CreateReport();
+            var path = Paths.get(REPORTS_PATH, String.format(FILE_NAME,LocalDate.now().getMonth())).toAbsolutePath();
+            return Files.readAllBytes(path);
+        }catch (IOException e){
+            throw  new RuntimeException();
+        }
     }
+
+    private void CreateReport(){
+        var worBook= new XSSFWorkbook();
+        var sheet = worBook.createSheet(SHEET_NAME);
+        sheet.setColumnWidth(0,5000);
+        sheet.setColumnWidth(1,7000);
+        sheet.setColumnWidth(2,3000);
+
+        var header = sheet.createRow(0);
+        var headerStyle = worBook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.VIOLET.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        var font = worBook.createFont();
+        font.setFontName(FONT_TYPE);
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        var headerCell = header.createCell(0);
+        headerCell.setCellValue(COLUMN_CUSTOMER_ID);
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell =  header.createCell(1);
+        headerCell.setCellValue(COLUMN_CUSTOMER_NAME);
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell =    header.createCell(2);
+        headerCell.setCellValue(COLUMN_CUSTOMER_PURCHASES);
+        headerCell.setCellStyle(headerStyle);
+
+        var style = worBook.createCellStyle();
+        style.setWrapText(true);
+
+        var customers = this.customerRepository.findAll();
+
+        var rowPos = 1;
+
+        for(CustomerEntity customer: customers) {
+            var row = sheet.createRow(rowPos);
+            var cell = row.createCell(0);
+            cell.setCellValue(customer.getDni());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(1);
+            cell.setCellValue(customer.getFullName());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(2);
+            cell.setCellValue(getTotalPurchase(customer));
+            cell.setCellStyle(style);
+
+            rowPos++;
+        }
+            var report = new File(String.format(REPORTS_PATH_WITH_NAME, LocalDate.now().getMonth()));
+            var path = report.getAbsolutePath();
+            var fileLocaiton = path+ FILE_TYPE;
+            try(var outputStream = new FileOutputStream(fileLocaiton)){
+                worBook.write(outputStream);
+                worBook.close();
+            }catch (IOException e){
+                log.error("Cant create Excel", e);
+                throw  new RuntimeException();
+            }
+        }
+
+
+
+    private static int getTotalPurchase(CustomerEntity customer) {
+        return customer.getTotalLodgings() + customer.getTotalFlights() + customer.getTotalTours();
+    }
+
+
+    private static final String SHEET_NAME = "Customer total sales";
+    private static final String FONT_TYPE = "Arial";
+    private static final String COLUMN_CUSTOMER_ID = "id";
+    private static final String COLUMN_CUSTOMER_NAME ="name";
+    private static final String COLUMN_CUSTOMER_PURCHASES ="purchases";
+    private static final String REPORTS_PATH_WITH_NAME = "reports/Sales-%s";
+    private static final String REPORTS_PATH = "reports";
+    private static final String FILE_TYPE =".xlsx";
+    private static final String FILE_NAME = "Sales-%s.xlsx";
 }
